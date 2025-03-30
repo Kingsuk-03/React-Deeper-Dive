25e06b17bed98b61019ccd011f73a43246b9de74
Kingsuk-03 <kingsukbose58@gmail.com>
2025-03-31 00:24:18 +0530

    React.Fragment, Temorary Solution of re-render

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,23 +1,23 @@
 import React, { useState } from "react";
 
-// function App() {
-//   const [name, setName] = useState("Arko");
+function App() {
+  const [name, setName] = useState("Arko");
 
-//   function updatename() {
-//     setName("My name is" + Math.floor(Math.random() * 10));
-//   }
-//   return (
-//     //React.Fragment or Fragment solves the issue--If you don’t wrap sibling elements in a single parent, React will throw a syntax error because JSX must return a single element.
-//     <React.Fragment>
-//       <button onClick={updatename}>Click me to change the Title</button>
-//       <Header title={name}>My </Header>
-//       <Header title="Arko"></Header>
-//     </React.Fragment>
-//   );
-// }
-// function Header({ title }) {
-//   return <div>{title}</div>;
-// }
+  function updatename() {
+    setName("My name is" + Math.floor(Math.random() * 10));
+  }
+  return (
+    //React.Fragment or Fragment solves the issue--If you don’t wrap sibling elements in a single parent, React will throw a syntax error because JSX must return a single element.
+    <React.Fragment>
+      <button onClick={updatename}>Click me to change the Title</button>
+      <Header title={name}>My </Header>
+      <Header title="Arko"></Header>
+    </React.Fragment>
+  );
+}
+function Header({ title }) {
+  return <div>{title}</div>;
+}
 //The problem is every time the first header re renders the total app and other components re-render,which isn't a good practice.
 
 //SOLUTION--1 [Declare the the state variable on in a specific function if already know where's it's gonna use]
