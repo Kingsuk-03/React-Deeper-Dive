18b6027868c6bb209812f57f5a253f08ff19732c
Kingsuk-03 <kingsukbose58@gmail.com>
2025-03-31 08:27:42 +0530

    React.memo

--- frontend/vite-project/src/App.jsx
+++ frontend/vite-project/src/App.jsx
@@ -1,18 +1,22 @@
 import React, { useState } from "react";
 
 function App() {
-  const [name, setName] = useState("Arko");
+  const [name, setName] = useState("My name is Arko");
 
   function updatename() {
-    setName("My name is" + Math.floor(Math.random() * 10));
+    setName("My name is" + Math.random());
   }
   return (
-    //React.Fragment or Fragment solves the issue--If you don’t wrap sibling elements in a single parent, React will throw a syntax error because JSX must return a single element.
-    <React.Fragment>
+    // <React.Fragment> or <Fragment> solves the issue--If you don’t wrap sibling elements in a single parent, React will throw a syntax error because JSX must return a single element.
+    <div>
       <button onClick={updatename}>Click me to change the Title</button>
-      <Header title={name}>My </Header>
+      <Header title={name}></Header>
       <Header title="Arko"></Header>
-    </React.Fragment>
+      <Header title="Arko"></Header>
+      <Header title="Arko"></Header>
+      <Header title="Arko"></Header>
+      <Header title="Arko"></Header>
+    </div>
   );
 }
 function Header({ title }) {
@@ -22,7 +26,7 @@ function Header({ title }) {
 
 //SOLUTION--1 [Declare the the state variable on in a specific function if already know where's it's gonna use]
 
-function App() {
+function SolutionApp1() {
   return (
     <div>
       <HeaderWithButton></HeaderWithButton>
@@ -50,5 +54,9 @@ function Header({ title }) {
   return <div>{title}</div>;
 }
 
-//SOLUTION--2 React.memo
+//SOLUTION--2 React.memo [ It only re-renders those components where the props(state variable) are changed ]
+
+const Header = React.memo(function Header({ title }) {
+  return <div>{title}</div>;
+});
 export default App;
