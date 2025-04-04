03aec2aa8da788f709e6d9881d905eadfb2c020b
Kingsuk-03 <kingsukbose58@gmail.com>
2025-04-04 22:58:30 +0530

    using useMemo

--- frontend/vite-project/src/UseMemo.jsx
+++ frontend/vite-project/src/UseMemo.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 // Without Using useMemo -------
 function App() {
@@ -9,6 +9,36 @@ function App() {
   for (let i = 1; i <= inputValue; i++) {
     count = count + i;
   }
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder={"Find Sum from 1 to n"}
+      ></input>
+      <br />
+      Sum form 1 to {inputValue} is {count}
+      <br />
+      <button onClick={() => setCounter(counter + 1)}>
+        Counter ({counter})
+      </button>
+    </div>
+  );
+}
+
+// Using useMemo
+
+function UseMemo() {
+  const [counter, setCounter] = useState(0);
+  const [inputValue, setInputValue] = useState(1);
+
+  let count = useMemo(() => {
+    let finalcount = 0;
+    for (let i = 1; i <= inputValue; i++) {
+      finalcount = finalcount + i;
+    }
+    return finalcount;
+  }, [inputValue]);
 
   return (
     <div>
