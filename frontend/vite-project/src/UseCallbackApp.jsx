38dd76d0eb03713b62182710756ecfd169410584
Kingsuk-03 <kingsukbose58@gmail.com>
2025-04-05 16:42:08 +0530

    useCallback

---
+++ frontend/vite-project/src/UseCallbackApp.jsx
@@ -0,0 +1,34 @@
+import { memo, useCallback, useState } from "react";
+
+function useCallbackApp() {
+  const [count, setCount] = useState(0);
+
+  const logSomething = useCallback(() => {
+    console.log("child clicked");
+  }, []);
+
+  return (
+    <div>
+      <ButtonComponent inputFunction={logSomething} />
+      <button
+        onClick={() => {
+          setCount(count + 1);
+        }}
+      >
+        Click me {count}
+      </button>
+    </div>
+  );
+}
+
+const ButtonComponent = memo(({ inputFunction }) => {
+  console.log("child render");
+
+  return (
+    <div>
+      <button onClick={inputFunction}>Button clicked</button>
+    </div>
+  );
+});
+
+export default useCallbackApp;
